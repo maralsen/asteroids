a6b63ebcab409c8de036088fb5acc7ab94accef0
maralsen <flabbybawls.game@gmail.com>
2026-07-27 13:42:07 +0200

    Decouple game speed from CPU speed.

--- main.py
+++ main.py
@@ -7,17 +7,25 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while(True):
         log_state()
+
         for event in pygame.event.get():
             pass
+
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
 
 if __name__ == "__main__":
     main()
