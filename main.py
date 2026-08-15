55113b0512b4c10b807851cff2dd2565fd6bcd9a
maralsen <flabbybawls.game@gmail.com>
2026-08-15 12:54:19 +0200

    Minor cleanup.

--- main.py
+++ main.py
@@ -13,34 +13,32 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    asteroidfields = pygame.sprite.Group()
+
     player.Player.containers = (updatable, drawable)
+    player_ship = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     asteroid.Asteroid.containers = (asteroids, updatable, drawable)
     asteroidfield.AsteroidField.containers = (updatable)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player_ship = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroidfield.AsteroidField()
-
-
 
     while(True):
         log_state()
 
-        # to start processing event queue and to keep program responsive
         for event in pygame.event.get():
-            pass
+                    if event.type == pygame.QUIT:
+                        return  
 
         screen.fill("black")
+
+        updatable.update(dt)
+
         for sprite in drawable:
             sprite.draw(screen)
-        updatable.update(dt)
-        pygame.display.flip()
-
-        dt = clock.tick(60) / 1000
 
         for ast in asteroids:
             if ast.collides_with(player_ship):
@@ -48,9 +46,9 @@ def main():
                 print("Game over!")
                 sys.exit()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return     
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
